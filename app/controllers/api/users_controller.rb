03b6b0231e0d08a01bbb830ce5602c1ab0032ea3
Nicole Hemenway <nkhemenway@gmail.com>
2017-05-20 16:34:32 -0700

    write users controller

---
+++ app/controllers/api/users_controller.rb
@@ -0,0 +1,28 @@
+class Api::UsersController < ApplicationController
+  def create
+    begin
+      @user = User.new(user_params)
+    rescue ActiveRecord::RecordInvalid => invalid
+      render json: invalid.record.errors.full_messages, status: 404
+    end
+
+    if @user && @user.save
+      log_in(@user)
+      render "api/users/show"
+    end
+  end
+
+  def show
+    @user = User.find_by(id: params[:userId])
+    render :show
+  end
+
+  def update
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:f_name, :l_name, :email, :password, :username)
+  end
+end
