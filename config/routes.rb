9320e88f998cae734a5ce9d65794c636effeec88
Nicole Hemenway <nkhemenway@gmail.com>
2017-05-20 17:03:08 -0700

    set up sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  namespace :api do
+    get 'sessions/create'
+  end
+
+  namespace :api do
+    get 'sessions/show'
+  end
+
+  namespace :api do
+    get 'sessions/destroy'
+  end
+
   root "static_pages#root"
   namespace :api, defaults: {format: :json} do
     resource :user, only: [:create, :show, :update]
