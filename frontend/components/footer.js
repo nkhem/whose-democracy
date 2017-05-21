33fbd9d3141be4568b3bce680457aa019909911a
Nicole Hemenway <nkhemenway@gmail.com>
2017-05-20 17:55:49 -0700

    fix github and linked in links

--- frontend/components/footer.js
+++ frontend/components/footer.js
@@ -6,10 +6,10 @@ class Footer extends React.Component {
     return (
       <div className='footer' id={this.props.id}>
         <a href="https://github.com/nkhem/gulp" target="_blank">
-          <i className="fa fa-github" aria-hidden="true"></i>
+          <i className="fa fa-github" aria-hidden="true"></i> github
         </a>
         <a href="https://www.linkedin.com/in/nkhem/" target="_blank">
-          <i className="fa fa-linkedin-square" aria-hidden="true"></i>
+          <i className="fa fa-linkedin-square" aria-hidden="true"></i> linkedin
         </a>
       </div>
     );
