464e167accfa7b7a34c7bbd9612f6cce837e27f9
Nicole Hemenway <nkhemenway@gmail.com>
2017-05-20 17:10:34 -0700

    setup frontend folders and write login, signup, and logout ajax requests in session_spi_util

--- frontend/whose_democracy.js
+++ frontend/whose_democracy.js
@@ -1,7 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/store';
+import Root from './components/root';
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
-  ReactDOM.render(<h1>Hello World</h1>, root);
+
+  let store;
+  if (window.currentUser) {
+    const preloadedState = { session: { currentUser: window.currentUser } };
+    store = configureStore(preloadedState);
+  } else {
+    store = configureStore();
+  }
+
+  ReactDOM.render(<Root store={ store }/>, root);
 });
