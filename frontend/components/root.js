295f13c0822098e83214bcbad32eeb56c35a7658
Nicole Hemenway <nkhemenway@gmail.com>
2017-05-20 17:17:58 -0700

    create root component

---
+++ frontend/components/root.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { Router, Route, IndexRoute, hashHistory } from 'react-router';
+
+import App from './app';
+import Home from './home';
+import SessionForm from './session_form';
+
+const Root = ({ store }) => {
+  const _redirectIfLoggedIn = (nextState, replace) => {
+    const currentUser = store.getState().session.currentUser;
+    if (currentUser) {
+      replace('/');
+    }
+  };
+
+  return (
+    <Provider store={ store }>
+      <Router history={ hashHistory }>
+        <Route path="/" component={ App }>
+          <IndexRoute component={ Home }/>
+          <Route path="login" component={ SessionForm } onEnter={ _redirectIfLoggedIn } />
+          <Route path="signup" component={ SessionForm } onEnter={ _redirectIfLoggedIn } />
+        </Route>
+      </Router>
+    </Provider>
+  );
+};
+
+export default Root;
