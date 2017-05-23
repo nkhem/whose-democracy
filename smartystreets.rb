2d4611375bc6a4daa4676a98941d19b6bd8ee9d7
Nicole Hemenway <nkhemenway@gmail.com>
2017-05-23 11:43:53 -0700

    add smartystreets API lookup class

---
+++ smartystreets.rb
@@ -0,0 +1,49 @@
+require 'smartystreets_ruby_sdk/static_credentials'
+require 'smartystreets_ruby_sdk/client_builder'
+require 'smartystreets_ruby_sdk/us_street/lookup'
+require 'byebug'
+
+AUTH = {
+  SMARTY_AUTH_ID: 'cc5d64ad-b025-c32a-b258-85386ab89678',
+  SMARTY_AUTH_TOKEN: 'OPH7pOoUwQh7a680WXhT',
+}
+
+class USStreetSingleAddressExample
+  def run
+    auth_id = AUTH[:SMARTY_AUTH_ID]
+    auth_token = AUTH[:SMARTY_AUTH_TOKEN]
+    credentials = StaticCredentials.new(auth_id, auth_token)
+
+    client = ClientBuilder.new(credentials).build_us_street_api_client
+
+    lookup = USStreet::Lookup.new
+    lookup.street = '4101 Park Blvd'
+    lookup.city = 'Palo Alto'
+    lookup.state = 'CA'
+
+    begin
+      client.send_lookup(lookup)
+    rescue SmartyException => err
+      puts err
+      return
+    end
+
+    result = lookup.result
+debugger
+    if result.nil?
+      puts 'No candidates. This means the address is not valid.'
+      return
+    end
+
+    first_candidate = result[0]
+
+    puts "Address is valid. (There is at least one candidate)\n"
+    puts "ZIP Code: #{first_candidate.components.zipcode}"
+    puts "County: #{first_candidate.metadata.county_name}"
+    puts "Latitude: #{first_candidate.metadata.latitude}"
+    puts "Longitude: #{first_candidate.metadata.longitude}"
+  end
+end
+
+example = USStreetSingleAddressExample.new
+example.run
