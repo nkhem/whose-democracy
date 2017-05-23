127c3c9b5106b19ef1e154704389158675557d06
Nicole Hemenway <nkhemenway@gmail.com>
2017-05-22 21:20:29 -0700

    debug selectedreps checkboxing

--- frontend/components/write_my_reps/select_reps_form.js
+++ frontend/components/write_my_reps/select_reps_form.js
@@ -4,11 +4,16 @@ import SelectRepsCheckBox from './select_reps_checkbox';
 class SelectRepsForm extends React.Component {
   constructor(props){
     super(props);
+    let firstSelectedReps = props.usersReps;
     this.state = {
-      selectedReps: props.usersReps
+      unselectedReps: [],
+      selectedReps: firstSelectedReps
     };
 
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleCheckboxChange = this.handleCheckboxChange.bind(this);
+    this.uncheckRep = this.uncheckRep.bind(this);
+    this.checkRep = this.checkRep.bind(this);
   }
 
   handleSubmit(e) {
@@ -32,16 +37,48 @@ class SelectRepsForm extends React.Component {
   }
 
   handleCheckboxChange(rep){
-    console.log('toggle',rep);
+    console.log('in handleCheckboxChange, selectedReps', this.state.selectedReps);
+    if (this.state.selectedReps.includes(rep)) {
+      this.uncheckRep(rep);
+    } else {
+      this.checkRep(rep);
+    }
+  }
+
+  uncheckRep(rep){
+    let nextSelectedReps = this.state.selectedReps.filter(selectedRep => selectedRep !== rep);
+    console.log('nextSelectedReps',nextSelectedReps);
+    let nextUnselectedReps = this.state.unselectedReps.push(rep);
+    this.setState({
+      unselectedReps: nextUnselectedReps,
+      selectedReps: nextSelectedReps,
+    });
+    console.log('unchecked rep', rep, this.state);
+  }
+
+  checkRep(rep){
+    console.log(this.state.unselectedReps);
+    console.log(this.state.selectedReps);
+    let nextUnselectedReps = this.state.unselectedReps.filter(unselectedRep => unselectedRep !== rep);
+    let nextSelectedReps = this.state.selectedReps.push(rep);
+    this.setState({
+      selectedReps: nextSelectedReps,
+      unselectedReps: nextUnselectedReps
+    });
+    console.log('unchecked rep', rep, this.state);
   }
 
   render() {
     return (
 			<form
         className='select-reps-form'
-        onSubmit={this.handleSubmit} id="user-address-form">
+        onSubmit={this.handleSubmit} id="user-checkress-form">
 
-        { this.props.usersReps.map(rep => <SelectRepsCheckBox rep={rep} key={rep} handleCheckboxChange={this.handleCheckboxChange}/>)}
+        { this.props.usersReps.map(rep => <SelectRepsCheckBox
+          rep={rep}
+          key={rep}
+          checked={this.state.selectedReps.includes(rep)}
+          handleCheckboxChange={this.handleCheckboxChange}/>)}
 
         <input className='submit-btn' type="submit" value='Write them!' />
   		</form>
