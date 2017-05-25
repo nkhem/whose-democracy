0704ceb1cd8f7ab6b2e03a98e08b9b6d4f89574b
Nicole Hemenway <nkhemenway@gmail.com>
2017-05-25 15:49:49 -0700

    get find senator by state abbrev dropdown working

--- frontend/components/search_bar.js
+++ frontend/components/search_bar.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+const ALL_STATES =  ['AK', 'AL', 'AR', 'AZ', 'CA', 'CO', 'CT', 'DC', 'DE', 'FL', 'GA', 'HI', 'IA', 'ID', 'IL', 'IN', 'KS', 'KY', 'LA', 'MA', 'MD', 'ME', 'MI', 'MN', 'MO', 'MS', 'MT', 'NC', 'ND', 'NE', 'NH', 'NJ', 'NM', 'NV', 'NY', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VA', 'VT', 'WA', 'WI', 'WV', 'WY'];
+
 class SearchBar extends React.Component {
   constructor(props) {
     super(props);
@@ -7,6 +9,7 @@ class SearchBar extends React.Component {
       searchTerm: ''
     };
     this.update = this.update.bind(this);
+    this.renderStateAbbrevOptions = this.renderStateAbbrevOptions.bind(this);
   }
 
   update(field) {
@@ -15,17 +18,29 @@ class SearchBar extends React.Component {
     });
   }
 
+  renderStateAbbrevOptions(){
+    return ALL_STATES.map( stateAbbrev => {
+      return (
+        <option
+          key={stateAbbrev}
+          value={stateAbbrev}
+          >
+          {stateAbbrev}
+        </option>
+      );
+    });
+  }
+
   render(){
     return (
       <div className='search-bar' id={this.props.id}>
+        <h3>Find Senator by state abbrev</h3>
         <form onSubmit={ e => this.props.handleSubmit(this.state.searchTerm)(e) }>
-          <input
-            type="text"
-            placeholder="Find Senator by state abbrev"
-            autoComplete="off"
-            value={this.state.searchTerm}
-            onChange={ this.update('searchTerm') }
-          />
+
+        <select value={this.state.searchTerm} onChange={this.update('searchTerm')}>
+          <option value=''>--</option>
+          {this.renderStateAbbrevOptions()}
+        </select>
           <button>
             <i className="fa fa-search" aria-hidden="true"></i>search
           </button>
@@ -33,8 +48,14 @@ class SearchBar extends React.Component {
       </div>
     );
   }
-
 }
 
-
 export default SearchBar;
+
+// <input
+//   type="text"
+//   placeholder="Find Senator by state abbrev"
+//   autoComplete="off"
+//   value={this.state.searchTerm}
+//    }
+// />
