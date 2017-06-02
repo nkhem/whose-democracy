ab6d0280b82d0290e022a2aeeb0c7f1f043fff9e
Nicole Hemenway <nkhemenway@gmail.com>
2017-06-02 12:08:03 -0700

    set up frames for rep index, rep index detail, and rep show components

---
+++ frontend/components/rep/rep_index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import RepIndexDetail from './rep_index_detail';
+
+class RepIndex extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <RepIndexDetail />
+      </div>
+    );
+  }
+
+}
+
+export default RepIndex;
---
+++ frontend/components/rep/rep_index_detail.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class RepIndexDetail extends React.Component {
+
+  render() {
+    return (
+      <div></div>
+    );
+  }
+
+}
+
+export default RepIndexDetail;
